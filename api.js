7dc4d19dd54de234447e1ed928d1fbe308c1b62e
Zachary Smith <billyzacsmith@gmail.com>
2015-12-20 16:56:10 -0700

    Move the remaining CRUD routes into api.js. Note that the operation that reads all posts is seperated from the operations that operate on a single post. This distinction is represented by api.posts vs api.post.

--- api.js
+++ api.js
@@ -13,5 +13,69 @@ module.exports = {
         }) // client.query
       }) // pg.connect
     } // read
-  } // posts
-}
+  }, // posts
+  post: {
+    create: function(request, response) {
+      var creationTime = Date.now()
+      pg.connect(connectionString, function(err, client, done) {
+          client.query('INSERT INTO post VALUES (default, $1, $2, $3, $4)',
+            [request.body.title,
+             request.body.body,
+             request.body.author,
+             creationTime],
+            function(err, results) {
+              if (err)
+                console.log(err)
+              done()
+              response.json(results)
+            }) // client.query
+        }) // pg.connect
+    }, // create
+    read: function(request, response) {
+      pg.connect(
+        connectionString,
+        function(err, client, done) {
+          client.query(
+            'SELECT * FROM post WHERE id=$1',
+            [request.params.id],
+            function(err, results) {
+              done()
+              response.json(results)
+            }) // client.query
+        }) // pg.connect
+    }, // read
+    update: function(request, response) {
+      pg.connect(
+        connectionString,
+        function(err, client, done) {
+          if (err)
+            console.log('Error on pg.connect:', err)
+          client.query(
+            'UPDATE post SET title=$2, body=$3 WHERE id=$1',
+            [request.params.id,
+            request.body.title,
+            request.body.body],
+            function(err, results) {
+              if (err)
+                console.log('Error on client.query:', err)
+              done()
+              response.json(results)
+            }) // client.query
+        }) // pg.connect
+    }, // update
+    delete: function(request, response) {
+      pg.connect(connectionString, function(err, client, done) {
+        client.query(
+          "DELETE FROM post WHERE id=$1",
+          [request.params.id],
+          function(err, results) {
+            if (err)
+              console.log(err)
+            done()
+            response.json(results)
+          }) // client.query
+      }) // pg.connect
+    } // delete
+  } // post
+
+} // module.exports
--- server.js
+++ server.js
@@ -8,84 +8,30 @@ var connectionString = 'postgres://localhost/gblog'
 app.use(bodyParser.urlencoded({ extended: false }))
 
 //======== Posts ========
-//Create
-app.post('/posts', function(request, response) {
-  console.log(request.body)
-  var creationTime = Date.now()
-  pg.connect(connectionString, function(err, client, done) {
-    client.query('INSERT INTO post VALUES (default, $1, $2, $3, $4)',
-    [request.body.title,
-     request.body.body,
-     request.body.author,
-     creationTime],
-    function(err, results) {
-      done()
-      if (err){
-        console.log('There was an error with the post query:', err)
-        return
-      }
-      response.json(results)
-    })
-  })
-})
-
-//Read all
+// Read all posts
 app.get('/posts', function(request, response) {
   api.posts.read(response)
 })
 
-// Read one post
-app.get('/posts/:id', function(request, response) {
-  pg.connect(
-    connectionString,
-    function(err, client, done) {
-      client.query(
-        'SELECT * FROM post WHERE id=$1',
-        [request.params.id],
-        function(err, results) {
-          done()
-          response.json(results)
-        })
-    })
+//Create a post
+app.post('/posts', function(request, response) {
+  api.post.create(request, response)
 })
 
-//Update
+// Read one post
+app.get('/posts/:id', function(request, response) {
+  api.post.read(request, response)
+})
+
+// Update
 app.put('/posts/:id', function(request, response) {
-  console.log(request.params)
-  pg.connect(
-    connectionString,
-    function(err, client, done) {
-      if (err)
-        console.log('Error on pg.connect:', err)
-      client.query(
-        'UPDATE post SET title=$2, body=$3 WHERE id=$1',
-        [request.params.id,
-        request.body.title,
-        request.body.body],
-        function(err, results) {
-          if (err)
-            console.log('Error on client.query:', err)
-          done()
-          response.json(results)
-        })
-    })
+  api.post.update(request, response)
 })
 
 //Delete
 app.delete('/posts/:id', function(request, response) {
-  pg.connect(
-    connectionString,
-    function(err, client, done) {
-      client.query(
-        'DELETE FROM post WHERE id=$1',
-        [request.params.id],
-        function(err, results) {
-          done()
-          response.json(results)
-        }) // client.query
-    }) // pg.connect
-}) // app.delete
-
+  api.post.delete(request, response)
+})
 
 app.listen(8080, function() {
   console.log('Listening on 8080')
