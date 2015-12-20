5c742473b22d026df3721352efd03f17760a6914
Zachary Smith <billyzacsmith@gmail.com>
2015-12-19 21:30:18 -0700

    A get request to '/posts' returns data from the database.

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 var express = require('express')
 var app = express()
+var pg = require('pg')
+var connectionString = 'postgres://localhost/gblog'
 
-app.get('/', function(request, response) {
-  response.json({
-    "hello": "world"
+//======== Posts ========
+//Create
+
+//Read
+app.get('/posts', function(request, response) {
+  pg.connect(connectionString, function(err, client, done) {
+    client.query('SELECT * FROM post', function(err, results) {
+      if (err)
+        console.log(err)
+      done() // What does this do?
+      response.json(results)
+    })
   })
 })
+
+//Update
+//Delete
 
 app.listen(8080, function() {
   console.log('Listening on 8080')
